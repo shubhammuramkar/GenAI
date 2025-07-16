8303ff20fc09299600ea55a45a43aec2d45ef4ab
Shubham <shubham@macs-MacBook-Pro.local>
2025-07-16 21:21:09 +0530

    basic code

--- langchain_projects/Project-1-LLM/1_llm_demo.py
+++ langchain_projects/Project-1-LLM/1_llm_demo.py
@@ -1,2 +1,14 @@
+
 from langchain_openai import OpenAI
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(
+    model = 'gpt-3.5-turbo-instruct',
+    # temperature=2 
+)
+
+result = llm.invoke("what is the capital of India")
+print(result)
